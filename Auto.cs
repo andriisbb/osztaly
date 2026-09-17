ee0dc2cd0c1884fb076a46e86a48c12129a85375
andriisbb <boldog.andras.2023d@telekimezotur.hu>
2026-09-17 11:15:13 +0200

    konstruktor

---
+++ Auto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace osztaly
+{
+    internal class Auto
+    {
+        private int gyartasiEv;
+        public string Modell { get; set; }
+        public string Marka { get; set; }
+        public int GyartasiEv
+        {
+            get { return gyartasiEv; }
+            set
+            {
+                if (value > 1886 && value <= 2026)
+                {
+                    gyartasiEv = value;
+                }
+                else gyartasiEv = 0;
+            }
+        }
+        public Auto(int ev, string modell, string marka)
+        {
+            this.GyartasiEv = GyartasiEv;
+            Modell = modell;
+            Marka = marka;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using osztaly;
+
+Auto kek = new Auto(2020, "Focus", "Ford");
+Auto zold = new Auto(3100, "Golf", "Volkswagen");
+
+Console.WriteLine($"Kék autó: {kek.Marka} {kek.Modell}, gyártási év: {kek.GyartasiEv}");
+
+Console.WriteLine($"Zöld autó: {zold.Marka} {zold.Modell}, gyártási év: {zold.GyartasiEv}");
